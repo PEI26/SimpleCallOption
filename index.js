b46ef7b7e381d0c6f2e9d871a8cfedef9d7d89a3
PEI26 <46730988+PEI26@users.noreply.github.com>
2019-07-07 17:57:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -54,10 +54,18 @@ async function setCurrentMarketPrice(amount){
         let value = await dbInstance.setSpotPrice(request.body)
         response.send("Success");  
      
-       
-      
-     
-   })
+  })
+server.get('/test', (req, res) => {
+    res.send('test endpoint. server running!')
+})
+
+server.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
    
    
    // server
